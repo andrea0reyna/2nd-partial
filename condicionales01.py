696c33c5e7f98d92c1c98ac1188058a498967ed5
andrea0reyna <145026191+andrea0reyna@users.noreply.github.com>
2023-09-26 08:36:32 -0600

    condicionales01.py

---
+++ condicionales01.py
@@ -0,0 +1,8 @@
+#Escribe un programa que que reciba el num del usuario si es positiv, negativo o cero 
+num = int(input("Escribe tu numero:"))
+if num > 0:
+  print("Tu número es positivo")
+elif num < 0:
+  print("Tu número es negativo")
+else:
+  print("Tu numero es 0 ")
